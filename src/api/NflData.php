d22107abbb95fee138a2fd04d08202b7079dca48
3brettb <3brettb@gmail.com>
2017-12-09 13:52:07 -0500

    Fix NFLData Requests

--- src/api/NflData.php
+++ src/api/NflData.php
@@ -9,33 +9,33 @@ use Fantasy\NFL\API\Query\QueryGroup;
 class NflData extends NFLAPI
 {
 
-    public static function UpdatePlayers()
+    public static function AllPlayers()
     {
         $instance = NflData::instance();
         $count = 1000;
         $query = QueryGroup::define()
             ->query("QB", $instance->get(Uri::WEEKRANKS, [
-                    'position' => PositionStrings::QB,
-                    'count' => $count
+                'position' => PositionStrings::QB,
+                'count' => $count
             ]))
             ->query("RB", $instance->get(Uri::WEEKRANKS, [
                 'position' => PositionStrings::RB,
                 'count' => $count
             ]))
             ->query("WR", $instance->get(Uri::WEEKRANKS, [
-                'position' => PositionStrings::RB,
+                'position' => PositionStrings::WR,
                 'count' => $count
             ]))
             ->query("TE", $instance->get(Uri::WEEKRANKS, [
-                'position' => PositionStrings::RB,
+                'position' => PositionStrings::TE,
                 'count' => $count
             ]))
             ->query("K", $instance->get(Uri::WEEKRANKS, [
-                'position' => PositionStrings::RB,
+                'position' => PositionStrings::K,
                 'count' => $count
             ]))
             ->query("DEF", $instance->get(Uri::WEEKRANKS, [
-                'position' => PositionStrings::RB,
+                'position' => PositionStrings::DEF,
                 'count' => $count
             ]));
         return $query->execute()->normalize()->get();
