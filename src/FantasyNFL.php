eb4c7eadd4ffacacfcddd6b49e0909ca27889c4e
3brettb <3brettb@gmail.com>
2018-01-28 19:18:52 -0500

    Connect FantasyNFL to FantasyData

--- src/FantasyNFL.php
+++ src/FantasyNFL.php
@@ -3,22 +3,26 @@
 namespace Fantasy\NFL;
 
 use Fantasy\NFL\FantasyNFL\Accessors as FantasyAccessors;
+use Fantasy\NFL\FantasyNFL\Common\FantasyData;
 use Fantasy\NFL\FantasyNFL\Modifiers as FantasyModifiers;
 use Fantasy\NFL\Fantasy\Models as FantasyModels;
+use Fantasy\NFL\FantasyNFL\Settings;
 
-class FantasyNFL
+class FantasyNFL extends FantasyData
 {
-
-    use FantasyAccessors, FantasyModifiers;
 
     public static function login($user)
     {
         $team = FantasyModels\Team::find($user->team_id);
         $league = FantasyModels\League::find($team->league_id);
-        self::setLeague($league->id);
-        self::setTeamId($team->id);
-        self::setSeason($league->season);
-        self::setWeek($league->week);
+        $season = $league->season;
+        $week = $league->week;
+        Settings::setLeagueId($league->id);
+        Settings::setTeamId($team->id);
+        Settings::setSeasonId($season->id);
+        Settings::setYear($season->year);
+        Settings::setWeekId($week->id);
+        Settings::setWeekNumber($week->number);
     }
 
 }
